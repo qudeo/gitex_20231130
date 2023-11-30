defe6ac3af3b53d834d9f0d0ba1e5efc065d0b60
qudeo <korapadug@gmail.com>
2023-11-30 10:40:40 +0900

    날씨추가

--- src/ex1/Example1.java
+++ src/ex1/Example1.java
@@ -4,7 +4,8 @@ public class Example1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("오늘은 목요일 입니다.");
+		System.out.println("오늘은 날씨가 춥네요.");
 	}
 
 }
